06c38088d484a13edc43dd353c8a452c31d770c4
Thiago Barros <86922706+Tbarros1996@users.noreply.github.com>
2021-10-25 21:10:21 -0300

    Add files via upload

---
+++ 500_Algoritimos_Resolvidos/Capitulo 1/exercicio1.py
@@ -0,0 +1,12 @@
+# 500 Exercícios Resolvidos com Python
+# Thiago Barros
+# Exercícios resolvidos com base no Livro - 500 Algoritimos Resolvidos (ANITA LOPES E GUTO GARCIA)
+# Algoritimo Numero 28
+
+# Algoritimo que no terminal :" É PRECISO FAZER TODOS OS ALGORITMOS PARA APRENDER"
+
+def algo():
+    print("É PRECISO FAZER TODOS \n OS EXERCICIOS PARA APRENDER")
+
+algo()
+
---
+++ 500_Algoritimos_Resolvidos/Capitulo_2/algoritmo_131.py
@@ -0,0 +1,24 @@
+# 500 Exercícios Resolvidos com Python
+# Thiago Barros
+# Exercícios resolvidos com base no Livro - 500 Algoritimos Resolvidos (ANITA LOPES E GUTO GARCIA)
+# Algoritimo Numero 131
+# Capitulo 2
+
+
+"""
+A turma de Programação 1, por ter muitos alunos, será dividida em dias de provas.
+Após um estudo feito pelo coordenador, decidiu-se dividi-la em três grupos.
+
+Fazer um algoritmo que leia o nome do aluno e indicar a sala em que ele deverá
+fazer as provas, tendo em vista a tabela a seguir e sabendo-se que todas as salas
+se encontram no bloco F
+
+A - K:sala 101
+L-N:sala 102
+O - Z:sala 103
+
+
+"""
+
+
+
