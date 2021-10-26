423332f41cf762959f0ec3fcce022009856903c8
Thiago Barros <86922706+Tbarros1996@users.noreply.github.com>
2021-10-25 21:08:09 -0300

    Delete Resolução_500_Algoritmos directory

--- Resolução_500_Algoritmos/exercicio2.py
+++
@@ -1,9 +0,0 @@
-# 500 Exercícios Resolvidos com Python
-# Thiago Barros
-# Exercícios resolvidos com base no Livro - 500 Algoritimos Resolvidos (ANITA LOPES E GUTO GARCIA)
-# Algoritimo Numero 29
-
-# Algoritmo que Imprime o Nome que o Usuario Digitar
-
-nome = str(input("Entre com o Seu Nome:\n"))
-print("Seu Nome É:\n", nome)
